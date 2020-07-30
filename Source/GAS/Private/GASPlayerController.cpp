b24807c5bc2f678b0d8e7376f94f47e2ad32d085
JanSeliv <janseliw@gmail.com>
2020-07-30 02:31:29 +0200

    Activating abilities by 1-5 keyboards keys

--- Source/GAS/Private/GASPlayerController.cpp
+++ Source/GAS/Private/GASPlayerController.cpp
@@ -7,3 +7,22 @@ void AGASPlayerController::AddAbilityToUI_Implementation(TSubclassOf<UGameplayAb
 {
 	// BP implementation
 }
+
+void AGASPlayerController::OnAbilityPressed_Implementation(int32 AbilityN)
+{
+	// BP implementation
+}
+
+// Allows the PlayerController to set up custom input bindings.
+void AGASPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	DECLARE_DELEGATE_OneParam(FOnAbilityActivated, int32);
+	for (int i = 1; i <= 5; ++i)
+	{
+		const FName ActionName{FString("Ability_0") + FString::FromInt(i)};
+		InputComponent->BindAction<FOnAbilityActivated>(ActionName, EInputEvent::IE_Pressed, this, &ThisClass::OnAbilityPressed, i).
+		                bConsumeInput = false;
+	}
+}
--- Source/GAS/Public/GASPlayerController.h
+++ Source/GAS/Public/GASPlayerController.h
@@ -20,4 +20,12 @@ public:
 	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "C++")
 	void AddAbilityToUI(TSubclassOf<class UGameplayAbilityBase> InAbilityBaseClass);
+
+protected:
+	/**  */
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "C++", meta = (BlueprintProtected))
+	void OnAbilityPressed(int32 AbilityN);
+
+	/** Allows the PlayerController to set up custom input bindings. */
+	virtual void SetupInputComponent() override;
 };
